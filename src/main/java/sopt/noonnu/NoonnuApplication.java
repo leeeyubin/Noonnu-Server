62915c58f80445965b30c64b0346997b858548ea
oOccasio <dhzktldh@gmail.com>
2025-11-17 15:10:13 +0900

    Spring initialize

---
+++ src/main/java/sopt/noonnu/NoonnuApplication.java
@@ -0,0 +1,13 @@
+package sopt.noonnu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoonnuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoonnuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sopt/noonnu/NoonnuApplicationTests.java
@@ -0,0 +1,13 @@
+package sopt.noonnu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoonnuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
