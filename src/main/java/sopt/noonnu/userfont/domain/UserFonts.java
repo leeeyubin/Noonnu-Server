d97944fc9c964a18e8b62497e3e8a33e03ae52be
oOccasio <dhzktldh@gmail.com>
2025-11-19 16:40:08 +0900

    refactor: #7 Like-Compare Fonts User-Fonts Integration

--- src/main/java/sopt/noonnu/userfont/domain/UserFonts.java
+++ src/main/java/sopt/noonnu/userfont/domain/UserFonts.java
@@ -2,6 +2,7 @@ package sopt.noonnu.userfont.domain;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import sopt.noonnu.font.domain.Font;
@@ -11,11 +12,18 @@ import sopt.noonnu.user.domain.User;
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-@Table(name = "compare_fonts", uniqueConstraints = @UniqueConstraint(columnNames = {"user_id", "font_id"}))
-public class CompareFonts extends BaseCreatedEntity {
+@Table(name = "user_fonts", uniqueConstraints = @UniqueConstraint(columnNames = {"user_id", "font_id"}))
+public class UserFonts extends BaseCreatedEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "isLiked", nullable = false)
+    private boolean isLiked;
+
+    @Column(name = "isCompared", nullable = false)
+    private boolean isCompared;
+
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
@@ -25,9 +33,15 @@ public class CompareFonts extends BaseCreatedEntity {
     @JoinColumn(name = "font_id", nullable = false)
     private Font font;
 
-    private CompareFonts(User user, Font font){
+    @Builder(access = AccessLevel.PRIVATE)
+    private UserFonts(final User user,
+                      final Font font,
+                      final boolean isLiked,
+                      final boolean isCompared) {
         this.user = user;
         this.font = font;
+        this.isLiked = isLiked;
+        this.isCompared = isCompared;
     }
 
 }
